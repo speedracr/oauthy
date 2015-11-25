6d81a7a4fcc1c0d7f58accfefb0300e263ebf16f
Jonas Knipper <jonas@robocomobo.com>
2015-11-25 13:42:00 +0100

    Initial setup

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'figaro'
+gem 'omniauth-twitter'
+gem 'omniauth-github'
 
 group :development, :test do
   gem 'byebug'
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, ENV['TWITTER_OAUTH_KEY'], ENV['TWITTER_OAUTH_SECRET']
+  provider :github, ENV['GITHUB_OAUTH_KEY'], ENV['GITHUB_OAUTH_SECRET']
+end
