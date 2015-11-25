1ca107f8d10de0a7c45f76d690e5ff4ec7fb0caa
Jonas Knipper <jonas@robocomobo.com>
2015-11-25 17:39:55 +0100

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-
   get 'home/profile'
+  get 'auth/:provider/callback', to: "sessions#create"
+  delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
 end
