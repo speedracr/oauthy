d783265cad2449a43acd3a6c43974325ccf250c5
Jonas Knipper <jonas@robocomobo.com>
2015-11-25 17:43:17 +0100

    Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   def self.sign_in_from_omniauth(auth)
     find_by(provider: auth['provider'], uid: auth['uid']) ||
-            create_user_from_omniauth auth
+            create_user_from_omniauth(auth)
   end
 
   def self.create_user_from_omniauth(auth)
