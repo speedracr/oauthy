e79d012695075eaed431d791f5aef0960faa4d77
Jonas Knipper <jonas@robocomobo.com>
2015-12-01 17:32:17 +0100

    Update sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/profile'
   get 'auth/:provider/callback', to: "sessions#create"
-  delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
+  get 'sign_out', to: "sessions#destroy", as: 'sign_out'
 end
