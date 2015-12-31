7c69c8ee9fe1417829843e81ae87eb26abc86d5c
Jonas Knipper <jonas@robocomobo.com>
2016-01-01 00:59:36 +0100

    Add vanishing alerts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+  setTimeout(function(){
+    $('#notice-wrapper').fadeOut("slow", function(){
+      $(this).remove();
+    })
+  }, 4500);
+});
