38d2eefc1e9dde00f699a9e260c330b20217212f
Jonas Knipper <jonas@robocomobo.com>
2016-04-16 16:50:36 +0200

    Add podio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :ensure_podio_login
 
   private
+    def ensure_podio_login
+      if session[:podio_access_token]
+        init_podio_client
+      end
+    end
+
+    def init_podio_client
+      Podio.setup(
+        api_url: 'https://api.podio.com',
+        api_key: ENV['PODIO_OAUTH_ID'],
+        api_secret: ENV['PODIO_OAUTH_SECRET'],
+        oauth_token: Podio::OauthToken.
+          new('access_token' => session[:podio_access_token], 'refresh_token' => session[:podio_refresh_token])
+      )
+    end
+
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
